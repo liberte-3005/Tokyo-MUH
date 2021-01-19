f5cdc0379cd51d03ce4a8aa20c7080a3812e6a1b
librte-3005 <3051800.s@gmail.com>
2021-01-19 14:48:57 +0900

    form調整

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class PostsController extends Controller
 {
     public function index(){
-        $posts = Post::all();
+        $posts = Post::latest()->get();
         return view('tokyo',['posts'=> $posts]);
     }
 
@@ -20,12 +20,18 @@ class PostsController extends Controller
         $pin_code = $request->input('pin_code');
 
         Post::create(["categories" => $categories, "users" => $users, "text" =>$text, "pin_code"=>$pin_code]);
-        $posts = Post::all();
+        $posts = Post::latest()->get();
         return view('tokyo',['posts'=> $posts]);
     }
 
-    public function delete(Request $request){
-        Post::find($request->id)->delete();
+    public function delete($id){
+        $posts = Post::findOrFail($id);
+        $posts->delete();
         return redirect('');
+    }
+
+    public function showCity(){
+        $posts = Post::findOrFail();
+        return view('');
     }
 }
